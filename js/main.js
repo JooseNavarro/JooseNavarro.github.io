5ff30d5b160a6d5942f521b93b777e252996708e
Joosenavarro <Joosenavarro@gmail.com>
2017-01-13 22:57:59 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+//Llamamos a las dependencias gracias a browserify
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+//Renderizamos
+ReactDOM.render(
+  //La etiqueta que vamos a enviar al html
+  <h1 class="text-center">Caminando con Reactjs!</h1>,
+
+  //Con document.getElementById  lo enviamos a nuestro #content
+  document.getElementById('conten')
+);
+
+//JSX
+const Contenido = React.createClass({
+  render : ()=>{
+    return(
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+  )}
+})
+
+ReactDOM.render(
+  //La etiqueta que vamos a enviar al html
+  <Contenido />,
+  //Con document.getElementById  lo enviamos a nuestro #contenPool
+  document.getElementById('contenPool')
+);
